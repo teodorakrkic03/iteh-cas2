d7570721f300db9384339e1b9f9bdfc99df694d7
Teodora Krkic <teodora.krkic03@gmail.com>
2024-10-13 14:18:15 +0200

    Logovanje na index.php

--- index.php
+++ index.php
@@ -1,3 +1,37 @@
+<?php
+
+require "dbBroker.php";
+require "model/user.php";
+
+session_start();
+if(isset($_POST['username']) && isset($_POST['password'])){
+    $uname = $_POST['username'];
+    $upass = $_POST['password'];
+
+    $korisnik = new User(null,$uname,$upass);
+    $odg = $korisnik->logInUser($korisnik,$conn);
+    //$odg = User::logInUser($korisnik,$conn)  -> drugi nacin, moze ovako jer je logInUser static
+
+    if($odg->num_rows==1){
+        echo 
+        `<script> 
+        console.log("Uspesno ste se prijavili"); 
+        <\script>`;
+
+        $_SESSION['user_id'] = $korisnik->id;
+        header('location: home.php');
+        exit();
+    }else{
+        echo 
+        `<script> 
+        console.log("Neuspesna prijava"); 
+        <\script>`;
+    }
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
