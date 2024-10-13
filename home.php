190cf8ddeb4bcca3184fef5e3ebd9c1c2b62dcbf
Teodora Krkic <teodora.krkic03@gmail.com>
2024-10-13 14:16:50 +0200

    Ispravljene greske na home stranici

--- home.php
+++ home.php
@@ -46,9 +46,6 @@
             </tr>
             </thead>
             <tbody>
-            <?php
-            while ($red = $result->fetch_array()) {
-                ?>
                 <tr>
                     <td><?php echo $red["predmet"] ?></td>
                     <td><?php echo $red["katedra"] ?></td>
@@ -62,9 +59,6 @@
                     </td>
 
                 </tr>
-                <?php
-            }
-            } ?>
             </tbody>
         </table>
         <div class="row" >
@@ -77,7 +71,7 @@
                 <button id="btn-obrisi" class="btn btn-danger" style="background-color: red; border: 1px solid white;">Obrisi</button>
             </div>
 
-            <div class="col-md-2" style="text-align: right>; color:" >
+            <div class="col-md-2" style="text-align: right" >
                     <button id="btn-sortiraj" class="btn btn-normal" onclick="sortTable()">Sortiraj</button>
                 </div>
 
